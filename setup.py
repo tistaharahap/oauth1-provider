3916a1ea689876586c12d1c4e7b8157fececd1d6
Batista Harahap <batista@bango29.com>
2013-06-05 19:12:39 +0700

    pip error fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='oauth1-provider',
-      version='0.2.0',
+      version='0.2.1',
       description='OAuth 1.0 Provider with Redis in Python',
       long_description=readme(),
       keywords='oauth redis xauth',
