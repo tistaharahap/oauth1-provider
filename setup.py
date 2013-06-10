bf948b47f61c38374b8a85adf7a50e23b0e0342a
Batista Harahap <batista@bango29.com>
2013-06-11 02:21:22 +0700

    fixed setuptool bug

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 def readme():
-    with open('README.md') as f:
+    with open('README') as f:
         return f.read()
 
-setup(name='oauth1-provider',
-      version='0.2.1',
+setup(name='oauth1',
+      version='0.3.0',
       description='OAuth 1.0 Provider with Redis in Python',
       long_description=readme(),
       keywords='oauth redis xauth',
@@ -13,8 +13,9 @@ setup(name='oauth1-provider',
       author='Batista Harahap',
       author_email='batista@bango29.com',
       license='MIT',
-      packages=['oauth1-provider'],
+      packages=['oauth1'],
       install_requires=[
           'flask',
-          'redis'],
+          'redis',
+          'hiredis'],
       zip_safe=False)
