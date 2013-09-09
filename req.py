b6d37338f4d9389514e408ac136c1c615e9dc238
Batista Harahap <batista@bango29.com>
2013-09-10 02:00:47 +0700

    heavy lifting with sql store, nonces are now registered
    need to change redis store's logic

--- req.py
+++ req.py
@@ -10,6 +10,6 @@ requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
 o = oauthnesia(base_url='http://localhost:5000/',
-               cons_key='OPTddWtrBbSaVoE3105RLjYyfIY3T3WDGRgvgKFs',
-               cons_sec='7gS45cQhJEHdfaPxJRqPt4HbFrfn5Gdgu2CK7LDg')
+               cons_key='8kO3kOBlpkkthW82RvIqtpsreccWvWJjUHq6mWKm',
+               cons_sec='Vps5E1FKrqDfTJ4vucGKeSvm41wjomOmpVZLdBrN')
 print o.xauth('username', 'password')
